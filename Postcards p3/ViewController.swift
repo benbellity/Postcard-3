b2464619e52f2aab3b2a97c57a5af0d954eb225e
BEN BELLITY <benjamin@Benjamins-MacBook-Pro.local>
2014-09-30 19:00:13 +0200

    Initial Commit

---
+++ Postcards p3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcards p3
+//
+//  Created by BEN BELLITY on 30/09/2014.
+//  Copyright (c) 2014 Famiis Online Apps LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
